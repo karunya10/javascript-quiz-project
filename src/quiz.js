84f29206e09034dda2e4bd0a5bac39efe1e8b404
karunya10 <karunya.prabha@gmail.com>
2025-07-12 22:16:05 +0200

    Mini Project 1

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,50 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  // 2. getQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    // 4. shuffleQuestions()
+  // 3. moveToNextQuestion()
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
 
-    // 5. checkAnswer(answer)
+  // 4. shuffleQuestions()
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.questions[i], this.questions[j]] = [
+        this.questions[j],
+        this.questions[i],
+      ];
+    }
+  }
 
-    // 6. hasEnded()
+  // 5. checkAnswer(answer)
+  checkAnswer(answer) {
+    if (this.questions[this.currentQuestionIndex].answer === answer) {
+      this.correctAnswers++;
+    }
+  }
+
+  // 6. hasEnded()
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    }
+    if ((this.currentQuestionIndex = this.questions.length)) {
+      return true;
+    }
+  }
 }
